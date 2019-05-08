914630790cfff75ec530e72e00e42a006c19b88e
qiukai <1369095024@qq.com>
2019-05-08 08:58:45 +0800

    second commit1.6

--- src/main/java/cn/edu/sicnu/cs/controller/check_attendance/EmployeeController.java
+++ src/main/java/cn/edu/sicnu/cs/controller/check_attendance/EmployeeController.java
@@ -6,19 +6,29 @@ import cn.edu.sicnu.cs.pojo.IdUtils;
 import cn.edu.sicnu.cs.service.check_attendance.DepartmentService;
 import cn.edu.sicnu.cs.service.check_attendance.EmployeeService;
 import cn.edu.sicnu.cs.service.check_attendance.IdUtilsService;
+import cn.edu.sicnu.cs.utils.ResultUtil;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author kaier
@@ -95,7 +105,7 @@ public class EmployeeController {
         request.getRequestDispatcher("/WEB-INF/pages/employee/delete_detail.jsp").forward(request,response);
     }
 
-    @RequestMapping("delete_info")
+    @RequestMapping("/delete_info")
     public String deleteToDB(@RequestParam(value="cid")String cid,
                            @RequestParam(value="uid")String uid,
                            @RequestParam(value = "name")String name,Model model
@@ -112,36 +122,69 @@ public class EmployeeController {
     }
 
     @RequestMapping("/addEmployee")
-    public void addEmployee(
-            @RequestParam(value = "name")String name,
-            @RequestParam(value = "title")String title,
-            @RequestParam(value = "salary")String salary,
-            @RequestParam(value = "department")String departmentName,HttpServletResponse response) throws IOException {
+    public @ResponseBody ResultUtil addEmployee(
+            HttpServletRequest request,HttpServletResponse response) throws IOException, FileUploadException {
+//        @RequestParam(value = "name")String name,
+//        @RequestParam(value = "title")String title,
+//        @RequestParam(value = "salary")String salary,
+//        @RequestParam(value = "department")String departmentName,
+//
 
-        response.setCharacterEncoding("UTF-8");
-        response.setContentType("text/html;charset=UTF-8");
+//        String name=null;
+//        String title=null;
+//        String salary=null;
+//        String departmentName=null;
+        System.out.println("debug......");
+        //获得磁盘文件条目工厂
+        DiskFileItemFactory factory = new DiskFileItemFactory();
+        //文件上传处理
+        ServletFileUpload upload = new ServletFileUpload(factory);
+        List<FileItem> list = upload.parseRequest(request);
+        //缓冲
+        byte[] tempBytes = new byte[1024*10];
+        ByteArrayOutputStream arrayOutputStream = new ByteArrayOutputStream();
+        Map<String,String> fieldMap = new HashMap<>(16);
+
+        for (FileItem fileItem : list) {
+            //只处理非表单类型
+            if(!fileItem.isFormField()){
+                InputStream is = fileItem.getInputStream();
+                int read = 0;
+                while((read = is.read(tempBytes))!=0){
+                    //获得图像的字节数组流,以便存入数据库
+                    arrayOutputStream.write(tempBytes,0,read);
+                }
+            }else{
+                fieldMap.put(fileItem.getFieldName(),fileItem.getString("UTF-8"));
+            }
+        }
+
+        System.out.println(fieldMap.get("name")+" "+fieldMap.get("title")+" "+fieldMap.get("salary")+" "+fieldMap.get("department"));
 
         //为员工生成id号
         IdUtils id = new IdUtils();
         idUtilsService.insert(id);
         //获取部门的id
-        Department department = departmentService.selectByName(departmentName);
+        Department department = departmentService.selectByName(fieldMap.get("department"));
         department.setPersonnelNum(department.getPersonnelNum()+1);
 
         Employee employee = new Employee();
         employee.setId(id.getId());
-        employee.setName(name);
-        employee.setTitle(title);
-        employee.setSalary(new BigDecimal(salary));
+        employee.setName(fieldMap.get("name"));
+        employee.setTitle(fieldMap.get("title"));
+        employee.setSalary(new BigDecimal(fieldMap.get("salary")));
         employee.setDepartmentId(department.getId());
+        employee.setImage(arrayOutputStream.toByteArray());
+        arrayOutputStream.close();
 
         int ret = employeeService.insert(employee);
+        ResultUtil resultUtil = new ResultUtil();
         if (ret<1){
             //错误提示
-            response.getWriter().print("false");
+            resultUtil.setResult("false");
         }
-
-        response.getWriter().close();
+        resultUtil.setResult("true");
+        return resultUtil;
     }
 
 }
---
+++ src/main/java/cn/edu/sicnu/cs/utils/ResultUtil.java
@@ -0,0 +1,19 @@
+package cn.edu.sicnu.cs.utils;
+
+/**
+ * @author kaier
+ * @date 2019-05-07 20:11
+ */
+public class ResultUtil {
+
+    private String result;
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+}
